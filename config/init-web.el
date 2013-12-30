52b84eda9952931a1f3492553d372cd41bf8ba54
Teodor Pripoae <teodor.pripoae@gmail.com>
2013-12-30 11:49:15 +0200

    Fixes web mode

--- config/init-web.el
+++ config/init-web.el
@@ -1,26 +1,27 @@
 (require-package 'coffee-mode)
-(require-package 'jade-mode)
+;(require-package 'jade-mode)
 
 
-(require-package 'skewer-mode)
-(skewer-setup)
+;(require-package 'skewer-mode)
+;(skewer-setup)
 
 
-(require-package 'rainbow-mode)
-(require 'rainbow-mode)
-(add-hook 'find-file-hook (lambda () (rainbow-turn-on)))
-
-
-(require-package 'emmet-mode)
-(add-hook 'css-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'web-mode-hook 'emmet-mode)
+;(require-package 'rainbow-mode)
+;(require 'rainbow-mode)
+;(add-hook 'find-file-hook (lambda () (rainbow-turn-on)))
 
 
 (require-package 'web-mode)
 (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
 
 ;; indent after deleting a tag
 (defadvice sgml-delete-tag (after reindent activate)
