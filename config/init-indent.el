bc384f759f6244839b8631704f5322beadd889fd
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-01-05 23:21:31 +0200

    Indent fixes

--- config/init-indent.el
+++ config/init-indent.el
@@ -1,8 +1,26 @@
 (require-package 'auto-indent-mode)
 (setq auto-indent-on-visit-file t)
-(setq auto-indent-key-for-end-of-line-then-newline "<M-return>")
-(setq auto-indent-key-for-end-of-line-insert-char-then-newline "<M-S-return>")
+;(setq auto-indent-key-for-end-of-line-then-newline "<M-return>")
+;(setq auto-indent-key-for-end-of-line-insert-char-then-newline "<M-S-return>")
 (require 'auto-indent-mode)
 (auto-indent-global-mode)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq ruby-indent-level 2)
+(setq css-indent-offset 2)
+(setq coffee-tab-width 2)
+
+(add-hook 'ruby-mode-hook
+  (function (lambda ()
+    (setq evil-shift-width ruby-indent-level))))
+
+(add-hook 'css-mode-hook
+  (function (lambda ()
+    (setq evil-shift-width css-indent-offset))))
+
+(add-hook 'coffee-mode-hook
+  (function (lambda ()
+    (setq evil-shift-width coffee-tab-width))))
 
 (provide 'init-indent)
