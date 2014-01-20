9c5aef92ceb1b75191be9a239048db8721f989fa
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-01-20 14:00:48 +0200

    Rails reloaded + rinari shortcuts

--- config/init-ruby.el
+++ config/init-ruby.el
@@ -40,4 +40,18 @@
 
 (add-hook 'ruby-mode-hook 'ri-bind-key)
 
+(define-key rinari-minor-mode-map (kbd "C-c t") 'rinari-find-test)
+(define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
+(define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
+(define-key rinari-minor-mode-map (kbd "C-c v") 'rinari-find-view)
+
+(setq load-path (cons (expand-file-name "~/.emacs.d/elisp/rails-reloaded") load-path))
+(require 'rails-autoload)
+
+;; Better indention for multi-line paren blocks
+(setq ruby-deep-indent-paren-style nil)
+
+(require-package 'bundler)
+(require 'bundler)
+
 (provide 'init-ruby)
