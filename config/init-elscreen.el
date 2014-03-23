07e57a9fb17115c8d36c1a2e710b0b6c34db22e6
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-03-23 16:30:15 +0200

    Different kbd shortcuts for elscreen for linux/mac

--- config/init-elscreen.el
+++ config/init-elscreen.el
@@ -1,4 +1,4 @@
-(package-install 'elscreen)
+(require-package 'elscreen)
 ;; (require 'elscreen)
 
 (setq elscreen-color-theme-override-theme t)
@@ -10,7 +10,12 @@
 (global-set-key (kbd "<f3>") 'elscreen-create)
 (global-set-key (kbd "<f4>") 'elscreen-kill)
 
-(global-set-key (kbd "s-{") 'elscreen-previous)
-(global-set-key (kbd "s-}") 'elscreen-next)
+(if (eq system-type 'darwin)
+  (progn
+    (global-set-key (kbd "s-{") 'elscreen-previous)
+    (global-set-key (kbd "s-}") 'elscreen-next))
+  (progn
+    (global-set-key (kbd "C-{") 'elscreen-previous)
+    (global-set-key (kbd "C-}") 'elscreen-next)))
 
 (provide 'init-elscreen)
