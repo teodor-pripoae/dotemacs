3539c96b83076a70326865c8cb15356736139415
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-03-25 17:23:09 +0200

    More helm

--- config/init-helm.el
+++ config/init-helm.el
@@ -3,11 +3,19 @@
 
 (require-package 'helm)
 (require-package 'helm-swoop)
+(require-package 'helm-rails)
+(require-package 'helm-rb)
+(require-package 'helm-themes)
 
 (after 'projectile
   (require-package 'helm-projectile))
 
 (require 'helm-config)
 (require 'helm-swoop)
+(require 'helm-rails)
+(require 'helm-rb)
+(require 'helm-themes)
+
+(global-set-key (kbd "C-c h") 'helm-mini)
 
 (provide 'init-helm)
