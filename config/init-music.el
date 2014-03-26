6262d9627df80df988583dd427ac0f6f032d702f
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-03-26 12:38:04 +0200

    Spotify controls (play/pause, prev, next)

--- config/init-music.el
+++ config/init-music.el
@@ -1,4 +1,27 @@
 (require-package 'helm-spotify)
 (require 'helm-spotify)
 
+;;;###autoload
+(defun spotify-play-pause ()
+  "Pause music in spotify"
+  (interactive)
+  (shell-command (format "osascript -e 'tell application %S to playpause '" "Spotify")))
+
+;;;###autoload
+(defun spotify-prev ()
+  "Play previous song in spotify"
+  (interactive)
+  (shell-command (format "osascript -e 'tell application %S to previous track '" "Spotify")))
+
+;;;###autoload
+(defun spotify-next ()
+  "Play next song in spotify"
+  (interactive)
+  (shell-command (format "osascript -e 'tell application %S to next track '" "Spotify")))
+
+
+(global-set-key (kbd "<f7>") 'spotify-prev)
+(global-set-key (kbd "<f8>") 'spotify-play-pause)
+(global-set-key (kbd "<f9>") 'spotify-next)
+
 (provide 'init-music)
